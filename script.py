8ddad6d7b8c2916a113e25027b7e8265dd3e2ee8
MikeLuceriaga <114997529+MikeLuceriaga@users.noreply.github.com>
2022-10-10 16:53:52 +0800

    Add files via upload

---
+++ script.py
@@ -0,0 +1,8 @@
+from flask import *
+app = Flask(__name__)
+
+@app.route('/table/<int:num>')
+def table(num):
+    return render_template('print-table.html',n=num)
+if __name__ == '__main__':
+    app.run(debug = True)
